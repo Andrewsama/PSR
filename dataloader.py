37f8ad7f0922da51ef6905ff5dab2fbfe0960f99
Andrewsama <42922103+Andrewsama@users.noreply.github.com>
2022-10-27 10:57:56 +0800

    PSR

--- dataloader.py
+++ dataloader.py
@@ -160,14 +160,14 @@ class GraphDataset:
         self.train_set = pd.read_csv(f'./data/preprocessed/{src}/train_set.txt')
         self.test_set = pd.read_csv(f'./data/preprocessed/{src}/test_set.txt')
         self.friendNet = pd.read_csv(f'./data/preprocessed/{src}/trust.txt')
-        # 获得总用户数和item数
+        # 
         self.n_user = pd.concat([self.train_set, self.test_set])['user'].nunique()
         self.m_item = pd.concat([self.train_set, self.test_set])['item'].nunique()
         self.trainUser = np.array(self.train_set['user']) # train user
-        self.trainUniqueUser = np.unique(self.train_set['user'])# 不重复的train user
+        self.trainUniqueUser = np.unique(self.train_set['user'])# 
         self.trainItem = np.array(self.train_set['item']) # train item
-        self._trainDataSize = len(self.train_set)   # 用于训练的user-item数
-        self._testDataSize = len(self.test_set)     # 用于测试的user-item数
+        self._trainDataSize = len(self.train_set)   # 
+        self._testDataSize = len(self.test_set)     # 
         print(f"{self._trainDataSize} interactions for training")
         print(f"{self._testDataSize} interactions for testing")
         print(f"Number of users: {self.n_user}\n Number of items: {self.m_item}")
@@ -175,16 +175,16 @@ class GraphDataset:
         print(f"{world.dataset} Rating Density: {(self._trainDataSize + self._testDataSize) / self.n_user / self.m_item}")
 
         # build (users,items), bipartite graph
-        self.interactionGraph = None   # 获得user-item的卷积核
-        # 构建稀疏的训练集user-item矩阵(csr)
+        self.interactionGraph = None   # 
+        # 
         self.UserItemNet = csr_matrix((np.ones(len(self.train_set)), (self.trainUser, self.trainItem)),
                                       shape=(self.n_user, self.m_item))
-        # list: 训练集    所有用户的items [[用户0的items],[用户1的items],...]
+        # list: 
         self._allPos = self.getUserPosItems(list(range(self.n_user)))
-        # dict: 测试集    key为用户，value为items {用户i:[items]}
+        # dict: 
         self._testDic = self.__build_test()
-        self._coldTestDic = self.__build_cold_test()# 删除训练集中高于20item的测试集user
-        # dict: 整个集合  user映射item，item映射user
+        self._coldTestDic = self.__build_cold_test()# 
+        # dict: 
         self._userDic, self._itemDic = self._getInteractionDic()
 
     @property
@@ -275,19 +275,19 @@ class GraphDataset:
             except IOError:
                 print("generating adjacency matrix")
                 start = time()
-                # 稀疏矩阵大小：(user数+item数) * (user数+item数)
+                # 
                 adj_mat = sp.dok_matrix((self.n_user + self.m_item, self.n_user + self.m_item), dtype=np.float32)
                 adj_mat = adj_mat.tolil()
                 R = self.UserItemNet.tolil()
 
-                adj_mat[:self.n_user, self.n_user:] = R    # 右上角u*i位置赋值
-                adj_mat[self.n_user:, :self.n_user] = R.T  # 左下角i*u位置赋值
+                adj_mat[:self.n_user, self.n_user:] = R    # 
+                adj_mat[self.n_user:, :self.n_user] = R.T  # 
                 adj_mat = adj_mat.todok()
 
                 rowsum = np.array(adj_mat.sum(axis=1))     # D
                 d_inv = np.power(rowsum, -0.5).flatten()   # D^-0.5
-                d_inv[np.isinf(d_inv)] = 0.                # 处理孤立点
-                d_mat = sp.diags(d_inv)                    # 对角化
+                d_inv[np.isinf(d_inv)] = 0.                # 
+                d_mat = sp.diags(d_inv)                    # 
 
                 # D^-0.5 * A * D^-0.5
                 norm_adj = d_mat.dot(adj_mat)
@@ -305,7 +305,7 @@ class GraphDataset:
         item_interaction = {}
 
         def getDict(_set):
-            for i in range(len(_set)):   # 遍历训练/测试集
+            for i in range(len(_set)):   # 
                 user = _set['user'][i]
                 item = _set['item'][i]
                 if user_interaction.get(user):
@@ -325,16 +325,15 @@ class GraphDataset:
 class SocialGraphDataset(GraphDataset):
     def __init__(self, src):
         super(SocialGraphDataset, self).__init__(src)
-        # 读取trust(连接关系)数据集
+        #
         self.friendNet = pd.read_csv(f'./data/preprocessed/{src}/trust.txt')
-        # 构建稀疏的邻接矩阵
+        # 
         self.socialNet = csr_matrix((np.ones(len(self.friendNet)), (self.friendNet['user'], self.friendNet['friend'])),
                                     shape=(self.n_user, self.n_user))
 
         self.interactionGraph = None
         self.socialGraph = None
 
-        # 输出连边数和密度
         print(f"Number of Links: {len(self.friendNet)}")
         print(f"{world.dataset} Link Density: {len(self.friendNet) / self.n_user / self.n_user}")
 
@@ -348,15 +347,14 @@ class SocialGraphDataset(GraphDataset):
                 print("generating adjacency matrix")
                 start = time()
                 R = self.UserItemNet.tolil()  #
-                '''修改'''
+                '''change'''
                 rowsum = np.array(R.sum(axis=1))
-                print('用户的平均item数为:',sum(rowsum) / len(rowsum))
+                print('The average number of item of each useer:',sum(rowsum) / len(rowsum))
                 d_inv = np.power(rowsum, -0.5).flatten()
                 # d_inv = np.power(rowsum, -1).flatten()
                 d_inv[np.isinf(d_inv)] = 0.
                 d_mat = sp.diags(d_inv)
-                # way1 : 获得偏好邻接矩阵卷积核（不去考虑朋友之间的度关系）
-                # 注：若两者没有相同的item，则会造成两个节点嵌入无值
+                # way1 
                 adj_mat = self.socialNet.tolil()
                 norm_adj = d_mat.dot(adj_mat)
                 norm_adj = norm_adj.dot(d_mat)
@@ -375,18 +373,18 @@ class SocialGraphDataset(GraphDataset):
                     edge += 1
                     # value.append(tmp_degree)
                     value.append(tmp_degree)
-                    # if tmp_degree == 0:   # 不要这个朋友
+                    # if tmp_degree == 0:   # 
                     #     user_mean[i] = user_mean[i] - 0.5
                     #     user_mean[j] = user_mean[j] - 0.5
                 avg_kk = kk / edge
-                # 用户初始平均13度
-                print('平均每条边对应user的共同items数为', avg_kk)
-                # print('用户的平均度为:',sum(user_mean) / len(user_mean))
+                # 
+                print(avg_kk)
+                # print('the average degree:',sum(user_mean) / len(user_mean))
                 # user_mean = np.power(user_mean, -1).flatten()
                 # user_mean[np.isinf(user_mean)] = 0.
                 # user_mean = sp.diags(user_mean)
 
-                # norm_adj = user_mean.dot(norm_adj)      # 取均值操作
+                # norm_adj = user_mean.dot(norm_adj)      # 
                 simNet = csr_matrix(
                     (np.array(value), (self.friendNet['user'], self.friendNet['friend'])),
                     shape=(self.n_user, self.n_user)).tolil()
@@ -398,10 +396,10 @@ class SocialGraphDataset(GraphDataset):
                 norm_adj = norm_adj.tocsr()
 
                 # adj_mat = self.socialNet.tolil()
-                # rowsum = np.array(adj_mat.sum(axis=1))   # 获得节点度D向量
-                # d_inv = np.power(rowsum, -0.5).flatten() # D^-0.5(铺平)
-                # d_inv[np.isinf(d_inv)] = 0.              # 避免孤立点经上一步后值为inf（变为0）
-                # d_mat = sp.diags(d_inv)                  # 将度向量转为矩阵
+                # rowsum = np.array(adj_mat.sum(axis=1))   # 
+                # d_inv = np.power(rowsum, -0.5).flatten() # D^-0.5
+                # d_inv[np.isinf(d_inv)] = 0.              # inf(0)
+                # d_mat = sp.diags(d_inv)                  # 
                 #
                 # # D^-0.5 * A * D^-0.5
                 # norm_adj = d_mat.dot(adj_mat)
@@ -424,7 +422,6 @@ class SocialGraphDataset(GraphDataset):
     #         except IOError:
     #             print("generating interaction matrix")
     #             start = time()
-    #             # 稀疏矩阵大小：(user数+item数) * (user数+item数)
     #             # adj_mat = sp.dok_matrix((self.n_user + self.m_item, self.n_user + self.m_item), dtype=np.float32)
     #             # adj_mat = adj_mat.tolil()
     #             R = self.UserItemNet.tolil().T#
@@ -442,11 +439,11 @@ class SocialGraphDataset(GraphDataset):
     #             print(f"costing {time() - start}s, saved norm_mat...")
     #             sp.save_npz(f'./data/preprocessed/{self.src}/interaction_adj_mat.npz', norm_inter)
     #
-    #         self.interactionGraph = _convert_sp_mat_to_sp_tensor(norm_inter)  # 将稀疏矩阵转为torch稀疏tensor
-    #         self.interactionGraph = self.interactionGraph.coalesce().to(world.device)  # 对多个相同索引求和操作
+    #         self.interactionGraph = _convert_sp_mat_to_sp_tensor(norm_inter)  # 
+    #         self.interactionGraph = self.interactionGraph.coalesce().to(world.device)  #
     #     return self.interactionGraph
 
-    def getDenseSocialGraph(self):    # 使用稠密矩阵加载
+    def getDenseSocialGraph(self):    # 
         if self.socialGraph is None:
             self.socialGraph = self.getSocialGraph().to_dense()
         else:
